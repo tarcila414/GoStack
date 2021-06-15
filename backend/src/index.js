9a6698b5b56f1495165733617183abb03641542d
Tarcila-Silva <tarcila.silva@igti.edu.br>
2021-06-15 14:04:37 -0300

    Test

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,24 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/projects', (req, res) => {
     return res.json({msg: "Hello World"});
 })
+
+app.post('/projects', (req, res) => {
+    return res.json({msg: "Hello World"});
+})
+
+app.put('/projects', (req, res) => {
+    return res.json({msg: "Hello World"});
+})
+
+
+app.get('/projects', (req, res) => {
+    return res.json({msg: "Hello World"});
+})
+
+
 
 app.listen(3333, () => {
     console.log('🎉 Back-end started!');
