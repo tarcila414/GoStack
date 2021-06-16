fc7a0d99e8c82f8c98d8f98571ddd48ca7f63323
tarcila414 <tarcila086@gmail.com>
2021-06-16 14:56:07 -0300

    Adding the image, css and file loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Header from './components/Header';
 
+import './App.css';
+
+import backgroundImg from './assets/img1.jpg'
 
 function App() {
     const projects = ['Desenvolvimento de App', 'Front-end'];
@@ -9,6 +12,7 @@ function App() {
     return (
         <>
             <Header />
+            <img width={300} src={backgroundImg}/>
             <ul>
                 { projects.map( project => <li key={project }>{project}</li>) }
             </ul>
