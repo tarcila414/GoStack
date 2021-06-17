5a6f59cdee7f9f1b04a522a90505c301598ccc5d
tarcila414 <tarcila086@gmail.com>
2021-06-17 14:31:54 -0300

    Adding the mobile app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,67 @@
 const express = require('express'); // para observar rotas
+const cors = require('cors');
+const { request } = require('express');
+
+const { uuid }  = require('uuidv4');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+const projects = [];
+
 app.get('/projects', (req, res) => {
-    return res.json({msg: "Hello World"});
+    const { title } = req.query;
+
+    const results = title 
+        ? projects.filter(project => project.title.includes(title))
+        : projects
+
+    return res.json(results);
 })
 
 app.post('/projects', (req, res) => {
-    return res.json({msg: "Hello World"});
+    const { title, owner } = req.body;
+
+    const project = { id: uuid(), title, owner };  
+
+    projects.push(project);
+
+    return res.json(project);
 })
 
-app.put('/projects', (req, res) => {
-    return res.json({msg: "Hello World"});
+app.put('/projects/:id', (req, res) => {
+    const  { id } = req.params;
+    const { title, owner } = req.body;
+
+
+    const projectIndex = projects.findIndex( project => project.id === id);
+
+    if( projectIndex < 0 ) {
+        return res.status(400).json({ error: 'Project not found'});
+    } 
+
+    const project = { id, title, owner };
+
+    projects[ projectIndex ] = project;
+
+    return res.json(project);
 })
 
 
-app.get('/projects', (req, res) => {
-    return res.json({msg: "Hello World"});
+app.delete('/projects/:id', (req, res) => {
+    const  { id } = req.params;
+    
+    const projectIndex = projects.findIndex( project => project.id === id);
+
+    if( projectIndex < 0 ) {
+        return res.status(400).json({ error: 'Project not found'});
+    } 
+
+    projects.splice(projectIndex, 1);
+
+    return res.status(204).send();
 })
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+
+import api from './services/api';
 
 import Header from './components/Header';
 
 import './App.css';
 
-import backgroundImg from './assets/img1.jpg'
+
 
 function App() {
-    const projects = ['Desenvolvimento de App', 'Front-end'];
+    const [ projects, setProjects ] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects').then( response => {
+            setProjects(response.data)
+        })
+    }, []);
+
+    async function handleAddProject () {
+        const response = await api.post('projects', {
+            'title': `Front-end com ReactJs ${Date.now()}`,
+            'owner': 'Maria Luisa'
+        }); 
+
+        const project = response.data;
+
+        setProjects([... projects, project]);
+    }
 
     return (
         <>
             <Header />
-            <img width={300} src={backgroundImg}/>
             <ul>
-                { projects.map( project => <li key={project }>{project}</li>) }
+                { projects.map( project => <li key={project.id}>{project.title}</li>) }
             </ul>
+
+            <button type="button" onClick={handleAddProject}>Add a new project</button>
         </>
     );
 }
