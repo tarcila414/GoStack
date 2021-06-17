92b46e772addea2cca802cdae87a9f9b836ec7dd
tarcila414 <tarcila086@gmail.com>
2021-06-17 19:05:26 -0300

    Started the implementation of the appointment requirement

--- GoBarber/backend/src/server.ts
+++ GoBarber/backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello world'});
-});
+app.use(express.json())
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('🎉 The server on port 3333')
-})
+});
+
