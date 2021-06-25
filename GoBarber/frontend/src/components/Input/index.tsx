b78c953b520aa2c1634054c535993b1bb69adc88
tarcila414 <tarcila086@gmail.com>
2021-06-25 17:38:31 -0300

    GoBarber-> Adding a border at the SignUp page input

--- GoBarber/frontend/src/components/Input/index.tsx
+++ GoBarber/frontend/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from "react";
+import React, { InputHTMLAttributes, useEffect, useState, useCallback, useRef } from "react";
 import { IconBaseProps } from "react-icons";
 import { Container } from "./styles";
 import { useField } from '@unform/core';
@@ -9,8 +9,21 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
 }
 
 const Input: React.FC<InputProps> = ({name, icon: Icon, ...propsResto}) => {
-    const inputRef = useRef(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [ isFocused, setIsFocused ] = useState(false);
+    const [ isFilled, setIsFilled ] = useState(false);
     const { fieldName, defaultValue, error, registerField } = useField(name);
+
+
+    const handleInputFocus = useCallback (() => {
+        setIsFocused(true);
+    }, []);
+
+    const handleInputBlur = useCallback (() => {
+        setIsFocused(false);
+
+        setIsFilled( !!inputRef.current?.value); // se houver algo no input setamos true
+    }, []);
     
     useEffect(() => {
         registerField({
@@ -21,9 +34,15 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...propsResto}) => {
     }, [fieldName, registerField]);
 
     return (
-        <Container>
+        <Container isFilled={isFilled} isFocused={ isFocused }>
             { Icon && <Icon  size={20} /> }
-            <input defaultValue={defaultValue} ref={ inputRef } {...propsResto} />
+            <input 
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                defaultValue={defaultValue} 
+                ref={ inputRef }
+                {...propsResto} 
+            />
         </Container>
     );
 }
