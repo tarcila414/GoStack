2d4b04e491c2dd74d6f19e7d94090d413a214b7f
tarcila414 <tarcila086@gmail.com>
2021-07-01 14:16:55 -0300

    GoBarber -> Backend: Implementation of the Dependency Invertion Principle on the Appointment route and the create Appointment service.

--- GoBarber/backend/src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
+++ GoBarber/backend/src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -1,14 +1,29 @@
+import { getRepository, Repository } from "typeorm";
+
 import IAppointmentsRepository from "@modules/appointments/repositories/IAppointmentsRepository";
-import { EntityRepository, Repository } from "typeorm";
-
 import Appointment from "../entities/Appointment";
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
 
-@EntityRepository(Appointment)
-class AppointmentRepository  extends Repository<Appointment> implements IAppointmentsRepository {
+
+class AppointmentRepository implements IAppointmentsRepository {
+    private ormRepository: Repository<Appointment>;
+    
+    constructor() {
+        this.ormRepository = getRepository(Appointment);
+    }
+
+    public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment> {
+        const appointment = this.ormRepository.create({ provider_id, date });
+
+        await this.ormRepository.save(appointment);
+
+        return appointment;
+    }
+
     public async findByDate(date: Date): Promise<Appointment | null>{
         // const findAppointment = this.appointments.find( appointment => isEqual(date, appointment.date));
 
-        const findAppointment = await this.findOne({
+        const findAppointment = await this.ormRepository.findOne({
             where: { date },
         })
         return findAppointment || null;
--- GoBarber/backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ GoBarber/backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -5,28 +5,31 @@ import Appointment from "../infra/typeorm/entities/Appointment";
 import AppointmentRepository from "../infra/typeorm/repositories/AppointmentRepository";
 
 import AppError from '@shared/errors/AppError';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface RequestDTO {
+interface IRequestDTO {
     provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({date, provider_id}:  RequestDTO): Promise<Appointment >{
+    constructor(
+       private appointmentsRepository: IAppointmentsRepository
+    ) {}
+
+    public async execute({date, provider_id}:  IRequestDTO): Promise<Appointment >{
         const appointmentsRepository = getCustomRepository(AppointmentRepository);
 
         const appointmentDate = startOfHour(date);
     
-        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
 
         if( findAppointmentInSameDate ) {
             throw new AppError('This is appointment is already booked');
             // return res.status(400).json({ message: });
         }
 
-        const appointment = appointmentsRepository.create({provider_id, date});
-    
-        await appointmentsRepository.save(appointment);
+        const appointment = await this.appointmentsRepository.create({provider_id, date});
 
         return appointment;
     };
