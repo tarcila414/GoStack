9efb93137b4771d2318146e4d7fbbd2f342fa9d1
tarcila414 <tarcila086@gmail.com>
2021-07-02 17:34:25 -0300

    GoBarber -> Backend: Update the CreateAppointment Test

--- GoBarber/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ GoBarber/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -10,6 +10,21 @@ describe( 'CreateAppointment', () => {
           fakeAppointmentsRepository,
         );
     
+        const appointment =  await createAppointment.execute({
+          date: new Date(),
+          provider_id: '123123',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('123123');
+      });
+
+      it('shouldn\'t be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+          fakeAppointmentsRepository,
+        );
+    
         const appointmentDate = new Date(2021,3,23,12);
 
         await createAppointment.execute({
@@ -25,9 +40,3 @@ describe( 'CreateAppointment', () => {
         ).rejects.toBeInstanceOf(AppError);
       });
 })
-
-// describe( 'CreateAppointment', () => {
-//     it('shouldn\'t be able to create a two appointments on the same date', () => {
-//         expect().toBe()
-//     })
-// })
